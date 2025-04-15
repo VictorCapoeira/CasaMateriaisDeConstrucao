0675dc81ae11dc4099d33ed23c9aeb4b18fb092b
Victor <victorgit28@outlook.com>
2025-04-15 10:32:35 -0300

    Minor Change

--- MateriaisDeConstruicao/Program.cs
+++ MateriaisDeConstruicao/Program.cs
@@ -5,11 +5,17 @@ var builder = WebApplication.CreateBuilder(args);
 var connectionString = "server=localhost;port=3306;database=produtosConstrucao;user=root;password=1234";
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySQL(connectionString));
 var app = builder.Build();
-
+app.UseStaticFiles();
 app.MapGet("/api/produtos", async (AppDbContext db) =>
 {
     var produtos = await db.Produtos.ToListAsync();
     return Results.Ok(produtos);
 });
+app.MapGet("/", context =>
+{
+    context.Response.Redirect("/index.html");
+    return Task.CompletedTask;
+});
+
 
 app.Run();
