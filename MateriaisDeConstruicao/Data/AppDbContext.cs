d2ad1dfd1106110d27ea1a62f63381de9f79e7bd
Victor <victorgit28@outlook.com>
2025-04-15 08:28:35 -0300

    Criado conteudo

---
+++ MateriaisDeConstruicao/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using CasaMateriaisDeConstrucao.Models;
+
+namespace CasaMateriaisDeConstrucao.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
