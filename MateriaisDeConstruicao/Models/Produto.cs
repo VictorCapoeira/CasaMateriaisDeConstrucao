19b8bdfb402242d5fd9d2aa4ad804890d60264a8
Victor <victorgit28@outlook.com>
2025-04-15 08:35:35 -0300

    Criado conexão com banco de dados

---
+++ MateriaisDeConstruicao/Models/Produto.cs
@@ -0,0 +1,12 @@
+namespace CasaMateriaisDeConstrucao.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string Imagem { get; set; }
+        public string Categoria { get; set; }
+    }
+}
